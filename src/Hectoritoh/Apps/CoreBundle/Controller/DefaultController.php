86c0e00b7fa4d1779fb09dbbafeba8fbd579165c
Hector Alvarado <halvarado@celmedia.com>
2014-03-10 16:24:21 -0500

    Creacion de Core

---
+++ src/Hectoritoh/Apps/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hectoritoh\Apps\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('HectoritohAppsCoreBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Hectoritoh/Apps/CoreBundle/HectoritohAppsCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hectoritoh\Apps\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HectoritohAppsCoreBundle extends Bundle
+{
+}
